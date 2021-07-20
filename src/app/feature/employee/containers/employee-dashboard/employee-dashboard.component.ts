a7461177d29f64ee9b0d05570124ff740036f359
lombold <colin.disler@gmail.com>
2021-07-20 10:17:36 +0200

    Adds forkJoin to dashboard
    The gui should be updated after health and employees are loaded

--- src/app/feature/employee/containers/employee-dashboard/employee-dashboard.component.ts
+++ src/app/feature/employee/containers/employee-dashboard/employee-dashboard.component.ts
@@ -1,27 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {EmployeeService} from "@core/services/employee.service";
 import {Employee} from "@core/domain/employee";
-import {Observable} from "rxjs";
-import {filter} from "rxjs/operators";
+import {forkJoin, Observable, Subject} from "rxjs";
+import {filter, takeUntil} from "rxjs/operators";
 import {ActivatedRoute, Router} from "@angular/router";
+import {HealthStatus} from "@core/domain/health-status";
+import {HealthService} from "@core/services/health.service";
 
 @Component({
   selector: 'app-employee-dashboard',
   templateUrl: './employee-dashboard.component.html',
   styleUrls: ['./employee-dashboard.component.scss']
 })
-export class EmployeeDashboardComponent implements OnInit {
+export class EmployeeDashboardComponent implements OnInit, OnDestroy {
 
   employeeToEdit!: Employee;
   employees$: Observable<Employee[]> = this.employeeService.getAll();
 
+  employees!: Employee[];
+  healthStatus!: HealthStatus;
+
+  private destroy$ = new Subject<void>();
+
   constructor(
     private employeeService: EmployeeService,
+    private healthService: HealthService,
     private router: Router,
     private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    forkJoin({
+      health: this.healthService.getHealth(),
+      employees: this.employeeService.getAll()
+    })
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe(({health, employees}) => {
+      this.healthStatus = health;
+      this.employees = employees;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   public showEmployee(employee: Employee) {
