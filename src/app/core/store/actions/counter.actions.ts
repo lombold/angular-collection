ff1a4958f9ab67477c65f0643932a9273a3dda03
lombold <colin.disler@gmail.com>
2021-08-04 10:02:25 +0200

    Adds basic counter actions
    for the stateful-click component

---
+++ src/app/core/store/actions/counter.actions.ts
@@ -0,0 +1,5 @@
+import {createAction} from "@ngrx/store";
+
+export const increment = createAction('[StatefulClick Component] Increment');
+export const decrement = createAction('[StatefulClick Component] Decrement');
+export const reset = createAction('[StatefulClick Component] Reset');
